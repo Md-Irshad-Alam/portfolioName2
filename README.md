e2a90570aeca4b9d2733ea3babefefe7a48100e5
Md irshad Alam <113013301+Md-Irshad-Alam@users.noreply.github.com>
2022-12-15 15:47:36 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Hi i am irshad alam and this is the readme file / this data i used for demo purpouse 
---
+++ src/data.js
@@ -0,0 +1,59 @@
+export const projects = [
+  {
+    title: "Alibaba Clone",
+    subtitle: "HTML CSS and JavaScript",
+    description:
+      "I clone Alibaba website, Alibaba Group Holding Limited, also known as Alibaba, is a Chinese multinational technology company specializing in e-commerce, retail, Internet, and technology.",
+    image: "./alibaba.png",
+    link: "https://velvety-fox-e0a352.netlify.app/product.html",
+  },
+  {
+    title: "PharmEasy Clone",
+    subtitle: "TML CSS and JavaScript",
+    description:
+      "PharmEasy is an Indian e-pharmacy company that sells medicines, diagnostics and telehealth online.",
+    image: "./pharmEasy.png",
+    link: "https://bespoke-jalebi-440ea8.netlify.app/",
+  },
+  {
+    title: "Mini YouTube",
+    subtitle: "HTML CSS and JavaScript",
+    description:
+      "This is Mini Youtube, In this App you Play the video , Sort the Video vie video length,  video category and duration. The App shows you default most tranding video on YouTube",
+    image: "./Youtube.png",
+    link: "https://heartfelt-douhua-6205b6.netlify.app/",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "Java",
+  
+];
